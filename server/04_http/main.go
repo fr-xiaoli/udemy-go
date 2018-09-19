9fa91dc6b091ac4aab0f4fab0b4b34402aa99cc9
fr-xiaoli <xiaoli.shen@fastretailing.com>
2018-09-19 23:45:15 +0900

    different routes

--- server/04_http/main.go
+++ server/04_http/main.go
@@ -54,37 +54,56 @@ func mux(conn net.Conn, ln string) {
 
 	// multiplexer
 	if m == "GET" && u == "/" {
-		index(conn)
+		index(u, conn)
 	}
 	if m == "GET" && u == "/about" {
-		about(conn)
+		about(u, conn)
 	}
 	if m == "GET" && u == "/contact" {
-		contact(conn)
+		contact(u, conn)
 	}
 	if m == "GET" && u == "/apply" {
-		apply(conn)
+		apply(u, conn)
 	}
 	if m == "POST" && u == "/apply" {
-		applyProcess(conn)
+		applyProcess(u, conn)
 	}
 }
 
-func index(conn net.Conn) {
-	body := `<!DOCTYPE html>
+func generateBody(uri string, hasApplyForm bool) string {
+	uri = strings.Replace(uri, "/", "", -1)
+	if uri == "" {
+		uri = "index"
+	}
+	title := strings.Title(uri)
+	formPlaceholder := ""
+	if hasApplyForm {
+		formPlaceholder = `<div>
+				<form method="post" action="/apply">
+					<input type="submit" value="apply">
+				</form>
+			</div>`
+	}
+	body := fmt.Sprintf(`<!DOCTYPE html>
 	<html lang="en">
 		<head>
 			<meta charset="UTF-8">
 			<title>Index</title>
 		</head>
 		<body>
-			<h1>Index</h1>
+			<h1>%s</h1>
 			<a href="/">index</a><br/>
 			<a href="/about">about</a><br/>
 			<a href="/contact">contact</a><br/>
 			<a href="/apply">apply</a>
+			%s
 		</body>
-	</html>`
+	</html>`, title, formPlaceholder)
+	return body
+}
+
+func index(uri string, conn net.Conn) {
+	body := generateBody(uri, false)
 	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
 	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
 	fmt.Fprint(conn, "Content-Type: text/html\r\n")
@@ -92,21 +111,8 @@ func index(conn net.Conn) {
 	fmt.Fprint(conn, body)
 }
 
-func about(conn net.Conn) {
-	body := `<!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<title>About</title>
-		</head>
-		<body>
-			<h1>About</h1>
-			<a href="/">index</a><br/>
-			<a href="/about">about</a><br/>
-			<a href="/contact">contact</a><br/>
-			<a href="/apply">apply</a>
-		</body>
-	</html>`
+func about(uri string, conn net.Conn) {
+	body := generateBody(uri, false)
 	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
 	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
 	fmt.Fprint(conn, "Content-Type: text/html\r\n")
@@ -114,21 +120,8 @@ func about(conn net.Conn) {
 	fmt.Fprint(conn, body)
 }
 
-func contact(conn net.Conn) {
-	body := `<!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<title>Contact</title>
-		</head>
-		<body>
-			<h1>Contact</h1>
-			<a href="/">index</a><br/>
-			<a href="/about">about</a><br/>
-			<a href="/contact">contact</a><br/>
-			<a href="/apply">apply</a>
-		</body>
-	</html>`
+func contact(uri string, conn net.Conn) {
+	body := generateBody(uri, false)
 	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
 	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
 	fmt.Fprint(conn, "Content-Type: text/html\r\n")
@@ -136,26 +129,8 @@ func contact(conn net.Conn) {
 	fmt.Fprint(conn, body)
 }
 
-func apply(conn net.Conn) {
-	body := `<!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<title>Apply</title>
-		</head>
-		<body>
-			<h1>Apply</h1>
-			<a href="/">index</a><br/>
-			<a href="/about">about</a><br/>
-			<a href="/contact">contact</a><br/>
-			<a href="/apply">apply</a><br/><br/>
-			<div>
-				<form method="post" action="/apply">
-					<input type="submit" value="apply">
-				</form>
-			</div>
-		</body>
-	</html>`
+func apply(uri string, conn net.Conn) {
+	body := generateBody(uri, true)
 	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
 	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
 	fmt.Fprint(conn, "Content-Type: text/html\r\n")
@@ -163,21 +138,8 @@ func apply(conn net.Conn) {
 	fmt.Fprint(conn, body)
 }
 
-func applyProcess(conn net.Conn) {
-	body := `<!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<title>Contact</title>
-		</head>
-		<body>
-			<h1>Apply Process</h1>
-			<a href="/">index</a><br/>
-			<a href="/about">about</a><br/>
-			<a href="/contact">contact</a><br/>
-			<a href="/apply">apply</a>
-		</body>
-	</html>`
+func applyProcess(uri string, conn net.Conn) {
+	body := generateBody(uri, false)
 	fmt.Fprint(conn, "HTTP/1.1 200 OK\r\n")
 	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
 	fmt.Fprint(conn, "Content-Type: text/html\r\n")
